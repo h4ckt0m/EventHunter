c4c147c7f9a67297afe0e99e0b08d18c827145a8
Arturo <arturito100500@gmail.com>
2020-05-14 16:32:35 +0200

    Todo el proyecto??

--- src/project/Main.java
+++ src/project/Main.java
@@ -6,16 +6,22 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main extends Application {
     public static Stage stage;
+    public static ArrayList<ArrayList> loggeduser;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         stage = primaryStage;
-        Parent root = FXMLLoader.load(getClass().getResource("scenes/Ventana Principal.fxml"));
-        stage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("scenes/VentanaPrincipal.fxml"));
+        stage.setTitle("Event Hunter");
         stage.setScene(new Scene(root));
         stage.show();
+
     }
 
 
